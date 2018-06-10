8f9258119236536c3a82f343bdee436fa081cc9c
Christian Oder <myself5@carbonrom.org>
2018-06-10 21:43:05 +0200

    maple: build gatekeeper and keymaster from source
    Change-Id: Icb611dd8953fcfeda85e9251a9f0b52cb52404d5

--- device/hardware.mk
+++ device/hardware.mk
@@ -1,3 +1,13 @@
 ### NFC
 PRODUCT_PACKAGES += \
     nfc_nci.msm8998
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
