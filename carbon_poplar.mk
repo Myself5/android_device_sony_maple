e3f45e9d2c62bcc0864a742e19e30553635f04d0
Christian Oder <myself5@carbonrom.org>
2019-08-18 22:58:27 +0200

    poplar: Add Carbon configs
    Change-Id: I7208497ac76ba2b6b0cfbe7e68855fa86a50ce41

--- carbon_poplar.mk
+++ carbon_poplar.mk
@@ -15,11 +15,12 @@ TARGET_SCREEN_WIDTH := 1080
 # vendor/lineage/config/common.mk
 TARGET_BOOTANIMATION_HALF_RES := true
 
-### LINEAGE
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+### Carbon
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_poplar
+PRODUCT_NAME := carbon_poplar
 PRODUCT_DEVICE := poplar
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := G8341
@@ -30,11 +31,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="G8341-user 9 47.2.A.11.228 3311891731 release-keys"
 
 BUILD_FINGERPRINT := Sony/G8341/G8341:9/47.2.A.11.228/3311891731:user/release-keys
-
-ifneq ($(LINEAGE_DEV_CERTIFICATE),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(LINEAGE_DEV_CERTIFICATE)
-endif
-
-ifneq ($(LINEAGE_VERITY_CERTIFICATE),)
-    PRODUCT_VERITY_SIGNING_KEY := $(LINEAGE_VERITY_CERTIFICATE)
-endif
